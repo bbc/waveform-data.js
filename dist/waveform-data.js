e346c8f6053e9fec68ee50f4d202c53f810aebc0
Kyle <kylewhite@gmail.com>
2014-06-05 11:34:52 -0400

    Suggested solution to bring in web-audio

--- dist/waveform-data.js
+++ dist/waveform-data.js
@@ -281,6 +281,97 @@ WaveformDataObjectAdapter.prototype = {
 },{}],4:[function(require,module,exports){
 "use strict";
 
+/* globals WaveformData, AudioContext, webkitAudioContext */
+
+// jshint -W098: true
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+/**
+ * Creates a working WaveformData based on binary audio data.
+ *
+ * This is still quite experimental and the result will mostly depend of the
+ * support state of the running browser.
+ *
+ * ```javascript
+ * var xhr = new XMLHttpRequest();
+ *
+ * // URL of a CORS MP3/Ogg file
+ * xhr.open("GET", "http://example.com/audio/track.ogg");
+ * xhr.responseType = "arraybuffer";
+ *
+ * xhr.addEventListener("load", function onResponse(progressEvent){
+ *   WaveformData.builders.webaudio(progressEvent.target.response, onProcessed(waveform){
+ *     console.log(waveform.duration);
+ *   });
+ * });
+ *
+ * xhr.send();
+ *  ```
+ *
+ * @todo use the errorCallback for `decodeAudioData` to handle possible failures
+ * @todo use a Web Worker to offload processing of the binary data
+ * @todo or use `SourceBuffer.appendBuffer` and `ProgressEvent` to stream the decoding
+ * @todo abstract the number of channels, because it is assumed the audio file is stereo
+ * @param {ArrayBuffer} raw_response
+ * @param {callback} what to do once the decoding is done
+ * @constructor
+ */
+module.exports = function fromAudioObjectBuilder(raw_response, callback){
+  var context = new AudioContext();
+  var scale = 512;
+  var scale_adjuster = 127; // to produce an 8bit like value
+
+  /*
+   * The result will vary on the codec implentation of the browser.
+   * We don't handle the case where the browser is unable to handle the decoding.
+   *
+   * @see https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html#dfn-decodeAudioData
+   *
+   * Adapted from BlockFile::CalcSummary in Audacity, with permission.
+   * @see https://code.google.com/p/audacity/source/browse/audacity-src/trunk/src/BlockFile.cpp
+   */
+  context.decodeAudioData(raw_response, function onAudioDecoded(audio_buffer){
+    var data_length = Math.floor(audio_buffer.length / scale);
+    var offset = 20;
+    var data_object = new DataView(new ArrayBuffer(offset + data_length * 2));
+    var left_channel, right_channel;
+    var min_value = Infinity, max_value = -Infinity, scale_counter = scale;
+    var buffer_length = audio_buffer.length;
+
+    data_object.setInt32(0, 1, true);   //version
+    data_object.setUint32(4, 1, true);   //is 8 bit
+    data_object.setInt32(8, audio_buffer.sampleRate, true);   //sample rate
+    data_object.setInt32(12, scale, true);   //scale
+    data_object.setInt32(16, data_length, true);   //length
+
+    left_channel = audio_buffer.getChannelData(0);
+    right_channel = audio_buffer.getChannelData(0);
+
+    for (var i = 0; i < buffer_length; i++){
+      var sample = (left_channel[i] + right_channel[i]) / 2 * scale_adjuster;
+
+      if (sample < min_value){
+        min_value = sample;
+      }
+
+      if (sample > max_value){
+        max_value = sample;
+      }
+
+      if (--scale_counter === 0){
+        data_object.setInt8(offset++, Math.floor(min_value));
+        data_object.setInt8(offset++, Math.floor(max_value));
+        min_value = Infinity; max_value = -Infinity; scale_counter = scale;
+      }
+    }
+
+    callback(new WaveformData(data_object.buffer, WaveformData.adapters.arraybuffer));
+  });
+};
+
+},{}],5:[function(require,module,exports){
+"use strict";
+
 var WaveformDataSegment = require("./segment.js");
 
 var WaveformDataPoint = require("./point.js");
@@ -883,7 +974,7 @@ WaveformData.adapter = function WaveformDataAdapter(response_data){
   this.data = response_data;
 };
 
-},{"./point.js":5,"./segment.js":6}],5:[function(require,module,exports){
+},{"./point.js":6,"./segment.js":7}],6:[function(require,module,exports){
 "use strict";
 
 /**
@@ -951,7 +1042,7 @@ WaveformDataPoint.prototype = {
     return this.context.in_offset(this.timeStamp);
   }
 };
-},{}],6:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
 "use strict";
 
 /**
@@ -1185,14 +1276,18 @@ WaveformDataSegment.prototype = {
     return this.visible ? this.context.offsetValues(this.offset_start, this.offset_length, 1) : [];
   }
 };
-},{}],7:[function(require,module,exports){
+},{}],8:[function(require,module,exports){
 "use strict";
 
 var WaveformData = require("./lib/core");
 WaveformData.adapters = require("./lib/adapters");
 
+WaveformData.builders = {
+  webaudio: require("./lib/builders/webaudio.js")
+};
+
 module.exports = WaveformData;
-},{"./lib/adapters":2,"./lib/core":4}]},{},[7])
-(7)
+},{"./lib/adapters":2,"./lib/builders/webaudio.js":4,"./lib/core":5}]},{},[8])
+(8)
 });
 ;
--- waveform-data.js
+++ waveform-data.js
@@ -3,4 +3,8 @@
 var WaveformData = require("./lib/core");
 WaveformData.adapters = require("./lib/adapters");
 
+WaveformData.builders = {
+  webaudio: require("./lib/builders/webaudio.js")
+};
+
 module.exports = WaveformData;
