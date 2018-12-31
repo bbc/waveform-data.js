96ca2a7bfdaa7dae9cd1f30cef871e840d314d85
Dave <davidairborne@gmail.com>
2018-12-31 13:42:39 -0800

    exposing waveform function for AudioBuffer

--- webaudio.js
+++ webaudio.js
@@ -1,5 +1,9 @@
 "use strict";
 
 var webAudioBuilder = require("./lib/builders/webaudio");
+var fromAudioBuffer = require("./lib/builders/audiobuffer");
+
+
+webAudioBuilder.fromAudioBuffer = fromAudioBuffer;
 
 module.exports = webAudioBuilder;
