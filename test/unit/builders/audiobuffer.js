252e9a1dc29c6f8273351e9df6f9efa7cfa4e860
Chris Needham <chris.needham@bbc.co.uk>
2019-01-13 15:58:41 +0000

    Fixed createFromAudio() tests

--- test/unit/builders/audiobuffer.js
+++ test/unit/builders/audiobuffer.js
@@ -1,18 +1,14 @@
 "use strict";
 
-/* globals describe, it, beforeEach, DOMException, __dirname */
+/* globals context, describe, it, beforeEach, DOMException, __dirname */
 
-var fromAudioBuffer = require("../../../lib/builders/audiobuffer");
-var AudioWaveform = require("../../../waveform-data");
+var WaveformData = require("../../../waveform-data");
 
 var chai = require("chai");
 var fs = require("fs");
-var sinonChai = require("sinon-chai");
 var expect = chai.expect;
 
-chai.use(sinonChai);
-
-describe("WaveformData AudioBuffer builder", function() {
+describe("WaveformData", function() {
   var sampleAudioBuffer;
   var AudioContext = window.AudioContext || window.webkitAudioContext;
   var audioContext = new AudioContext();
@@ -26,86 +22,90 @@ describe("WaveformData AudioBuffer builder", function() {
     });
   });
 
-  describe("factory", function() {
-    it("should return a valid waveform in case of success", function(done) {
-      var result = fromAudioBuffer(sampleAudioBuffer, function(err, waveform) {
-        expect(err).to.not.be.ok;
-        expect(waveform).to.be.an.instanceOf(AudioWaveform);
-        expect(waveform).to.have.property("offset_length");
+  describe(".createFromAudio", function() {
+    context("given an AudioBuffer", function() {
+      it("should return a valid waveform in case of success", function(done) {
+        var options = {
+          audio_buffer: sampleAudioBuffer
+        };
 
-        // file length: 88200 samples
-        // scale: 512 (default)
-        // 88200 / 512 = 172, with 136 samples remaining, so 173 points total
-        expect(waveform.offset_length).to.equal(173);
-        done();
+        WaveformData.createFromAudio(options, function(err, waveform) {
+          expect(err).to.not.be.ok;
+          expect(waveform).to.be.an.instanceOf(WaveformData);
+          expect(waveform.channels).to.equal(1);
+
+          // file length: 88200 samples
+          // scale: 512 (default)
+          // 88200 / 512 = 172, with 136 samples remaining, so 173 points total
+          expect(waveform.length).to.equal(173);
+          done();
+        });
       });
 
-      if (result && "then" in result) {
-        result.catch(console.error.message);
-      }
-    });
+      it("should adjust the length of the waveform when using a different scale", function(done) {
+        var options = {
+          audio_buffer: sampleAudioBuffer,
+          scale: 128
+        };
 
-    it("should adjust the length of the waveform when using a different scale", function(done) {
-      var options = { scale: 128 };
+        WaveformData.createFromAudio(options, function(err, waveform) {
+          expect(err).to.not.be.ok;
+          expect(waveform).to.be.an.instanceOf(WaveformData);
+          expect(waveform.channels).to.equal(1);
 
-      var result = fromAudioBuffer(sampleAudioBuffer, options, function(err, waveform) {
-        expect(err).to.not.be.ok;
-        expect(waveform).to.have.property("offset_length");
-
-        // file length: 88200 samples
-        // scale: 128
-        // 88200 / 128 = 689, with 8 samples remaining, so 690 points total
-        expect(waveform.offset_length).to.equal(690);
-        done();
+          // file length: 88200 samples
+          // scale: 128
+          // 88200 / 128 = 689, with 8 samples remaining, so 690 points total
+          expect(waveform.length).to.equal(690);
+          done();
+        });
       });
 
-      if (result && "then" in result) {
-        result.catch(console.error.message);
-      }
-    });
+      it("should return waveform data points", function(done) {
+        var options = {
+          audio_buffer: sampleAudioBuffer
+        };
 
-    it("should return waveform data points", function(done) {
-      var result = fromAudioBuffer(sampleAudioBuffer, function(err, waveform) {
-        expect(err).to.not.be.ok;
+        WaveformData.createFromAudio(options, function(err, waveform) {
+          expect(err).to.not.be.ok;
 
-        expect(waveform.min[0]).to.equal(-23);
-        expect(waveform.max[0]).to.equal(22);
+          expect(waveform.channel(0).min_sample(0)).to.equal(-23);
+          expect(waveform.channel(0).max_sample(0)).to.equal(22);
 
-        expect(waveform.min[waveform.offset_length - 1]).to.equal(-23);
-        expect(waveform.max[waveform.offset_length - 1]).to.equal(22);
-        done();
+          expect(waveform.channel(0).min_sample(waveform.length - 1)).to.equal(-23);
+          expect(waveform.channel(0).max_sample(waveform.length - 1)).to.equal(22);
+          done();
+        });
       });
 
-      if (result && "then" in result) {
-        result.catch(console.error.message);
-      }
-    });
+      it("should return correctly scaled waveform data points", function(done) {
+        var options = {
+          audio_buffer: sampleAudioBuffer,
+          amplitude_scale: 2.0
+        };
 
-    it("should return correctly scaled waveform data points", function(done) {
-      var options = { amplitude_scale: 2.0 };
+        WaveformData.createFromAudio(options, function(err, waveform) {
+          expect(err).to.not.be.ok;
 
-      var result = fromAudioBuffer(sampleAudioBuffer, options, function(err, waveform) {
-        expect(err).to.not.be.ok;
+          expect(waveform.channel(0).min_sample(0)).to.equal(-45);
+          expect(waveform.channel(0).max_sample(0)).to.equal(44);
 
-        expect(waveform.min[0]).to.equal(-45);
-        expect(waveform.max[0]).to.equal(44);
-
-        expect(waveform.min[waveform.offset_length - 1]).to.equal(-45);
-        expect(waveform.max[waveform.offset_length - 1]).to.equal(44);
-        done();
+          expect(waveform.channel(0).min_sample(waveform.length - 1)).to.equal(-45);
+          expect(waveform.channel(0).max_sample(waveform.length - 1)).to.equal(44);
+          done();
+        });
       });
 
-      if (result && "then" in result) {
-        result.catch(console.error.message);
-      }
-    });
+      it("should return an error if the scale_adjuster parameter is present", function() {
+        var options = {
+          audio_buffer: sampleAudioBuffer,
+          scale_adjuster: 127
+        };
 
-    it("should return an error if the scale_adjuster parameter is present", function() {
-      var options = { scale_adjuster: 127 };
-
-      expect(function() {
-        fromAudioBuffer(sampleAudioBuffer, options);
-      }).to.throw(Error, /scale_adjuster/);
+        expect(function() {
+          WaveformData.createFromAudio(options);
+        }).to.throw(Error, /scale_adjuster/);
+      });
     });
   });
 });
--- test/unit/builders/webaudio.js
+++ test/unit/builders/webaudio.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/* globals describe, it, beforeEach, DOMException, __dirname */
+/* globals context, describe, it, beforeEach, DOMException, __dirname */
 
 var WaveformData = require("../../../waveform-data");
 
@@ -25,123 +25,155 @@ describe("WaveformData", function() {
   });
 
   describe(".createFromAudio", function() {
-    it("should throw if audioContext is not the first argument", function() {
+    it("should throw if an AudioContext is given as the first argument", function() {
       expect(function() {
-        WaveformData.createFromAudio(new ArrayBuffer(), sinon.spy());
+        WaveformData.createFromAudio(audioContext, sinon.spy());
       }).to.throw(/AudioContext/);
     });
 
-    it("should return an error if the audio buffer is invalid", function(done) {
-      WaveformData.createFromAudio(audioContext, new ArrayBuffer(1024), function(err, waveform) {
-        expect(err).to.be.an.instanceOf(DOMException);
-        expect(waveform).to.not.be.ok;
+    context("given an AudioContext and ArrayBuffer", function() {
+      it("should return an error if the audio buffer is invalid", function(done) {
+        var options = {
+          audio_context: audioContext,
+          array_buffer: new ArrayBuffer(1024)
+        };
 
-        done();
+        WaveformData.createFromAudio(options, function(err, waveform) {
+          expect(err).to.be.an.instanceOf(DOMException);
+          expect(waveform).to.not.be.ok;
+
+          done();
+        });
       });
-    });
 
-    it("should return a valid waveform in case of success", function(done) {
-      WaveformData.createFromAudio(audioContext, sampleBuffer, function(err, waveform) {
-        expect(err).to.not.be.ok;
-        expect(waveform).to.be.an.instanceOf(WaveformData);
-        expect(waveform.channels).to.equal(1);
+      it("should return a valid waveform in case of success", function(done) {
+        var options = {
+          audio_context: audioContext,
+          array_buffer: sampleBuffer
+        };
 
-        // file length: 88200 samples
-        // scale: 512 (default)
-        // 88200 / 512 = 172, with 136 samples remaining, so 173 points total
-        expect(waveform.length).to.equal(173);
-        done();
+        WaveformData.createFromAudio(options, function(err, waveform) {
+          expect(err).to.not.be.ok;
+          expect(waveform).to.be.an.instanceOf(WaveformData);
+          expect(waveform.channels).to.equal(1);
+
+          // file length: 88200 samples
+          // scale: 512 (default)
+          // 88200 / 512 = 172, with 136 samples remaining, so 173 points total
+          expect(waveform.length).to.equal(173);
+          done();
+        });
       });
-    });
 
-    it("should adjust the length of the waveform when using a different scale", function(done) {
-      var options = { scale: 128 };
+      it("should adjust the length of the waveform when using a different scale", function(done) {
+        var options = {
+          audio_context: audioContext,
+          array_buffer: sampleBuffer,
+          scale: 128
+        };
 
-      WaveformData.createFromAudio(audioContext, sampleBuffer, options, function(err, waveform) {
-        expect(err).to.not.be.ok;
-        expect(waveform).to.be.an.instanceOf(WaveformData);
-        expect(waveform).to.have.property("offset");
-        expect(waveform.channels).to.equal(1);
+        WaveformData.createFromAudio(options, function(err, waveform) {
+          expect(err).to.not.be.ok;
+          expect(waveform).to.be.an.instanceOf(WaveformData);
+          expect(waveform.channels).to.equal(1);
 
-        // file length: 88200 samples
-        // scale: 128
-        // 88200 / 128 = 689, with 8 samples remaining, so 690 points total
-        expect(waveform.length).to.equal(690);
-        done();
+          // file length: 88200 samples
+          // scale: 128
+          // 88200 / 128 = 689, with 8 samples remaining, so 690 points total
+          expect(waveform.length).to.equal(690);
+          done();
+        });
       });
-    });
 
-    it("should return waveform data points", function(done) {
-      WaveformData.createFromAudio(audioContext, sampleBuffer, function(err, waveform) {
-        expect(err).to.not.be.ok;
+      it("should return waveform data points", function(done) {
+        var options = {
+          audio_context: audioContext,
+          array_buffer: sampleBuffer
+        };
 
-        expect(waveform.channels).to.equal(1);
+        WaveformData.createFromAudio(options, function(err, waveform) {
+          expect(err).to.not.be.ok;
 
-        expect(waveform.channel(0).min_sample(0)).to.equal(-23);
-        expect(waveform.channel(0).max_sample(0)).to.equal(22);
+          expect(waveform.channels).to.equal(1);
 
-        expect(waveform.channel(0).min_sample(waveform.offset_length - 1)).to.equal(-23);
-        expect(waveform.channel(0).max_sample(waveform.offset_length - 1)).to.equal(22);
-        done();
+          expect(waveform.channel(0).min_sample(0)).to.equal(-23);
+          expect(waveform.channel(0).max_sample(0)).to.equal(22);
+
+          expect(waveform.channel(0).min_sample(waveform.length - 1)).to.equal(-23);
+          expect(waveform.channel(0).max_sample(waveform.length - 1)).to.equal(22);
+          done();
+        });
       });
-    });
 
-    it("should return correctly scaled waveform data points", function(done) {
-      var options = { amplitude_scale: 2.0 };
+      it("should return correctly scaled waveform data points", function(done) {
+        var options = {
+          audio_context: audioContext,
+          array_buffer: sampleBuffer,
+          amplitude_scale: 2.0
+        };
 
-      WaveformData.createFromAudio(audioContext, sampleBuffer, options, function(err, waveform) {
-        expect(err).to.not.be.ok;
+        WaveformData.createFromAudio(options, function(err, waveform) {
+          expect(err).to.not.be.ok;
 
-        expect(waveform.channel(0).min_sample(0)).to.equal(-45);
-        expect(waveform.channel(0).max_sample(0)).to.equal(44);
+          expect(waveform.channel(0).min_sample(0)).to.equal(-45);
+          expect(waveform.channel(0).max_sample(0)).to.equal(44);
 
-        expect(waveform.channel(0).min_sample(waveform.length - 1)).to.equal(-45);
-        expect(waveform.channel(0).max_sample(waveform.length - 1)).to.equal(44);
-        done();
+          expect(waveform.channel(0).min_sample(waveform.length - 1)).to.equal(-45);
+          expect(waveform.channel(0).max_sample(waveform.length - 1)).to.equal(44);
+          done();
+        });
       });
-    });
 
-    it("should return multiple channels of waveform data points", function(done) {
-      var options = { split_channels: true };
+      it("should return multiple channels of waveform data points", function(done) {
+        var options = {
+          audio_context: audioContext,
+          array_buffer: sampleBuffer,
+          split_channels: true
+        };
 
-      WaveformData.createFromAudio(audioContext, sampleBuffer, options, function(err, waveform) {
-        expect(err).to.not.be.ok;
+        WaveformData.createFromAudio(options, function(err, waveform) {
+          expect(err).to.not.be.ok;
 
-        expect(waveform.channels).to.equal(4);
+          expect(waveform.channels).to.equal(4);
 
-        expect(waveform.channel(0).min_sample(0)).to.equal(-1);
-        expect(waveform.channel(0).max_sample(0)).to.equal(0);
+          expect(waveform.channel(0).min_sample(0)).to.equal(-1);
+          expect(waveform.channel(0).max_sample(0)).to.equal(0);
 
-        expect(waveform.channel(1).min_sample(0)).to.equal(-1);
-        expect(waveform.channel(1).max_sample(0)).to.equal(0);
+          expect(waveform.channel(1).min_sample(0)).to.equal(-1);
+          expect(waveform.channel(1).max_sample(0)).to.equal(0);
 
-        expect(waveform.channel(2).min_sample(0)).to.equal(-90);
-        expect(waveform.channel(2).max_sample(0)).to.equal(89);
+          expect(waveform.channel(2).min_sample(0)).to.equal(-90);
+          expect(waveform.channel(2).max_sample(0)).to.equal(89);
 
-        expect(waveform.channel(3).min_sample(0)).to.equal(-1);
-        expect(waveform.channel(3).max_sample(0)).to.equal(0);
+          expect(waveform.channel(3).min_sample(0)).to.equal(-1);
+          expect(waveform.channel(3).max_sample(0)).to.equal(0);
 
-        expect(waveform.channel(0).min_sample(waveform.length - 1)).to.equal(-1);
-        expect(waveform.channel(0).max_sample(waveform.length - 1)).to.equal(0);
+          expect(waveform.channel(0).min_sample(waveform.length - 1)).to.equal(-1);
+          expect(waveform.channel(0).max_sample(waveform.length - 1)).to.equal(0);
 
-        expect(waveform.channel(1).min_sample(waveform.length - 1)).to.equal(-1);
-        expect(waveform.channel(1).max_sample(waveform.length - 1)).to.equal(0);
+          expect(waveform.channel(1).min_sample(waveform.length - 1)).to.equal(-1);
+          expect(waveform.channel(1).max_sample(waveform.length - 1)).to.equal(0);
 
-        expect(waveform.channel(2).min_sample(waveform.length - 1)).to.equal(-90);
-        expect(waveform.channel(2).max_sample(waveform.length - 1)).to.equal(89);
+          expect(waveform.channel(2).min_sample(waveform.length - 1)).to.equal(-90);
+          expect(waveform.channel(2).max_sample(waveform.length - 1)).to.equal(89);
 
-        expect(waveform.channel(3).min_sample(waveform.length - 1)).to.equal(-1);
-        expect(waveform.channel(3).max_sample(waveform.length - 1)).to.equal(0);
-        done();
+          expect(waveform.channel(3).min_sample(waveform.length - 1)).to.equal(-1);
+          expect(waveform.channel(3).max_sample(waveform.length - 1)).to.equal(0);
+          done();
+        });
       });
-    });
 
-    it("should return an error if the scale_adjuster parameter is present", function() {
-      var options = { scale_adjuster: 127 };
+      it("should return an error if the scale_adjuster parameter is present", function() {
+        var options = {
+          audio_context: audioContext,
+          array_buffer: sampleBuffer,
+          scale_adjuster: 127
+        };
 
-      expect(function() {
-        WaveformData.createFromAudio(audioContext, sampleBuffer, options);
-      }).to.throw(Error, /scale_adjuster/);
+        expect(function() {
+          WaveformData.createFromAudio(options);
+        }).to.throw(Error, /scale_adjuster/);
+      });
     });
   });
 });
